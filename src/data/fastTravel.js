df4d95139e464c81c79ea260f26d44d468551cb5
xenix1337 <xenix1337gaming@gmail.com>
2026-01-05 23:41:34 +0100

    Updated fast travel locations and teporary added english names as comments

--- src/data/fastTravel.js
+++ src/data/fastTravel.js
@@ -1,35 +1,39 @@
 export const fastTravelMarkers = [
-  { name: "Chatka Hermita", x: 202, y: 400 },
-  { name: "Druidzki wybieg", x: 246, y: 90 },
-  { name: "Dundull", x: 390, y: 348 },
-  { name: "Dwór Srebrnej Polany", x: 141, y: 178 },
-  { name: "Dyniowa Farma Jaspera", x: 118, y: 149 },
-  { name: "Farma Burza", x: 87, y: 297 },
-  { name: "Farma Południowego Kopyta", x: 264, y: 350 },
-  { name: "Farma Słonecznikowych Pól", x: 26, y: 237 },
-  { name: "Farma Steve'a", x: 204, y: 193 },
-  { name: "Fort Pinta", x: 238, y: 266 },
-  { name: "Gospodarstwo Birka", x: 323, y: 258 },
-  { name: "Jarlaheim", x: 41, y: 237 },
-  { name: "Jezioro Valedale", x: 318, y: 125 },
-  { name: "Kryjówka Mistrza Szpiegowania", x: 236, y: 78 },
-  { name: "Księżycowy Sierp", x: 93, y: 330 },
-  { name: "Lodowy Portal", x: 320, y: 10 },
-  { name: "Miasteczko Srebrnej Polany", x: 184, y: 199 },
-  { name: "Posterunek Sekwoi", x: 367, y: 250 },
-  { name: "Ranczo Starshine", x: 302, y: 228 },
-  { name: "Stajnia Moorland", x: 188, y: 241 },
-  { name: "Stajnia Nowej Grani", x: 157, y: 348 },
-  { name: "Stajnia Złotych Liści", x: 32, y: 90 },
-  { name: "Stodoła klasyki Marleya", x: 175, y: 179 },
-  { name: "Tor wyścigowy Baronowej", x: 171, y: 133 },
-  { name: "Ujeżdżalnia", x: 118, y: 176 },
-  { name: "Wioska Jodłowy Gaj", x: 337, y: 175 },
-  { name: "Wioska Pięknego Ogona", x: 405, y: 197 },
-  { name: "Wioska Valedale", x: 272, y: 108 },
-  { name: "Wyspa Padok", x: 22, y: 273 },
-  { name: "Wzgórze Nilmera", x: 151, y: 221 },
-  { name: "Zachodni Przylądek", x: 42, y: 127 },
-  { name: "Zajazd pod Wilkiem", x: 142, y: 253 },
-  { name: "Zapomniane Pola", x: 87, y: 207 },
+  { name: "Chatka Hermita", x: 202, y: 400 }, // Hermit's Cottage
+  { name: "Druidzki wybieg", x: 246, y: 90 }, // Druid Paddock
+  { name: "Dundull", x: 390, y: 348 }, // Dundull
+  { name: "Dwór Srebrnej Polany", x: 141, y: 178 }, // Silverglade Manor
+  { name: "Dyniowa Farma Jaspera", x: 118, y: 149 }, // Jasper's Pumpkin Farm
+  { name: "Farma Burza", x: 87, y: 297 }, // Stormgarden
+  { name: "Farma Południowego Kopyta", x: 264, y: 350 }, // South Hoof Farm
+  { name: "Farma Słonecznikowych Pól", x: 26, y: 237 }, // Sunfield Farm
+  { name: "Farma Steve'a", x: 204, y: 193 }, // Steve's Farm
+  { name: "Fort Pinta", x: 238, y: 266 }, // Fort Pinta
+  { name: "Gospodarstwo Birka", x: 323, y: 258 }, // Birk's Grange
+  { name: "Jarlaheim", x: 41, y: 237 }, // Jarlaheim
+  { name: "Jezioro Valedale", x: 318, y: 125 }, // Valedale Lake
+  { name: "Kryjówka Mistrza Szpiegowania", x: 236, y: 78 }, // Spymaster's hideout
+  { name: "Księżycowy Sierp", x: 93, y: 330 }, // Crescent Moon Village
+  { name: "Lodowy Portal", x: 320, y: 10 }, // Icengate
+  { name: "Miasteczko Srebrnej Polany", x: 184, y: 199 }, // Silverglade Village
+  { name: "Posterunek Sekwoi", x: 367, y: 250 }, // Redwood Point
+  { name: "Ranczo Starshine", x: 302, y: 228 }, // Starshine Ranch
+  { name: "Stajnia Moorland", x: 188, y: 241 }, // Moorland Stables
+  { name: "Stajnia Nowej Grani", x: 157, y: 348 }, // New Hillcrest Stables
+  { name: "Stajnia Złotych Liści", x: 32, y: 90 }, // Goldenleaf Stables
+  { name: "Stodoła klasyki Marleya", x: 175, y: 179 }, // Marley's Vintage Barn
+  { name: "Tor wyścigowy Baronowej", x: 171, y: 133 }, // Baroness' Racetrack
+  { name: "Ujeżdżalnia", x: 118, y: 176 }, // Riding Arena
+  { name: "Wioska Jodłowy Gaj", x: 337, y: 175 }, // Firgrove Village
+  { name: "Wioska Pięknego Ogona", x: 405, y: 197 }, // Tailtop Village
+  { name: "Wioska Valedale", x: 272, y: 108 }, // Valedale Village
+  { name: "Wyspa Padok", x: 22, y: 273 }, // Paddock Island
+  { name: "Wzgórze Nilmera", x: 151, y: 221 }, // Nilmer's Highland
+  { name: "Zachodni Przylądek", x: 42, y: 127 }, // Cape West Fishing Village
+  { name: "Zajazd pod Wilkiem", x: 142, y: 253 }, // Wolf Hall Inn
+  { name: "Zapomniane Pola", x: 87, y: 207 }, // The Forgotten Fields
+  { name: "Plaża Fortu Pinta", x: 244, y: 260 }, // Fort Pinta Beach
+  { name: "Średniowieczny obóz", x: 89, y: 173 }, // Medieval Camp
+  { name: "Obozowisko Nica Stoneground", x: 308, y: 49 }, // Nic Stoneground's Camp
+  { name: "Pola Everwind", x: 182, y: 161 }, // The Everwind Fields
 ];
